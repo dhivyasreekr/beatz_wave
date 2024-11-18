bda6c75943b9808580954ee004712cf3b6ece13d
vprabhu1771 <dhivyasreekr@gmail.com>
2024-11-18 18:32:50 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'package:beatz_wave/services/Auth.dart';
 import 'package:flutter/material.dart';
 import 'package:beatz_wave/screens/HomeScreen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  // runApp(const MyApp());
+
+  runApp(
+    /// Providers are above [MyApp] instead of inside it, so that tests
+    /// can use [MyApp] while mocking the providers
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Auth()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
