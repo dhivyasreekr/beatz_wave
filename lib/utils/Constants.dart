8788c915e80a2c64119ee847b5e726b46f5b47b3
dhivyasreekr <dhivyasreekr@gmail.com>
2024-11-22 17:37:39 +0530

    Update Constants.dart

--- lib/utils/Constants.dart
+++ lib/utils/Constants.dart
@@ -1,6 +1,6 @@
 class Constants {
 
-  static const String BASE_URL = "http://192.168.0.102:8000/api";
+  static const String BASE_URL = "http://192.168.0.101:8000/api";
 
   static const String LOGIN_ROUTE = "/login";
 
@@ -11,5 +11,13 @@ class Constants {
   static const String USER_INFO_ROUTE ="/user";
 
   static const String GENRE_ROUTE ="/genres";
+
+  static const String BRAND_ROUTE="/brands";
+
+  static const String SONG_ROUTE="/songs";
+
+  static const String SONG_FILTER_BY_GENRE_ROUTE="/songs?genre_id=";
+
+  static const String SONG_FILTER_BY_BRAND_ROUTE="/songs?brand_id=";
 
 }
